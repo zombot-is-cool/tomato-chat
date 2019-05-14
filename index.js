dae3dc82d5977983d40aba91c52b786e6539c843
zombot-is-cool <chasesikula@gmail.com>
2019-05-14 18:44:48 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('t0MATO p3rS0n c0nn3ctd');
+    socket.on ('chat', function(message) {
+        io.emit('chat', message);
+    });
+    socket.on('disconnect', function() {
+        console.log('someone disconnected D:')
+    });
+} );
+http.listen(3000, function() {
+    console.log("tomato chat");
+});
